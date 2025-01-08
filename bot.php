b8f8b629e3f6976b772a0ca950daacfb6e9882ae
Phạm Nguyên Hồng <hongichik321@gmail.com>
2025-01-08 10:00:13 +0700

    update

--- bot.php
+++ bot.php
@@ -5,22 +5,25 @@ $update = file_get_contents("php://input");
 $update = json_decode($update, true);
 
 $chatId = $update['message']['chat']['id'];
-$message = $update['message']['text'];
+$message = trim($update['message']['text']); // Loại bỏ khoảng trắng thừa
 
-$url = "https://api.telegram.org/bot$botToken/sendMessage";
-$data = [
-    'chat_id' => $chatId,
-    'text' => "Chat ID của bạn là: $chatId"
-];
+// Kiểm tra nếu tin nhắn là "ID"
+if (strcasecmp($message, "ID") === 0) { // So sánh không phân biệt hoa thường
+    $url = "https://api.telegram.org/bot$botToken/sendMessage";
+    $data = [
+        'chat_id' => $chatId,
+        'text' => "Chat ID của bạn là: $chatId"
+    ];
 
-$options = [
-    'http' => [
-        'header' => "Content-Type: application/json\r\n",
-        'method' => 'POST',
-        'content' => json_encode($data),
-    ],
-];
+    $options = [
+        'http' => [
+            'header' => "Content-Type: application/json\r\n",
+            'method' => 'POST',
+            'content' => json_encode($data),
+        ],
+    ];
 
-$context = stream_context_create($options);
-file_get_contents($url, false, $context);
+    $context = stream_context_create($options);
+    file_get_contents($url, false, $context);
+}
 ?>
