6599fe6e498c50ff4f51ecd09a496fd499c94f16
Phạm Nguyên Hồng <hongichik321@gmail.com>
2025-01-11 10:49:48 +0700

    update

--- autogui.php
+++ autogui.php
@@ -3,7 +3,6 @@
 <?php
 // Kiểm tra nếu form được gửi đi
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Duyệt qua tất cả các phần tử trong $_POST và in ra giá trị
     foreach ($_POST as $key => $value) {
         if ($key == 'sdt') {
             echo "SĐT: " . $value . "<br>";
@@ -11,8 +10,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "Họ tên: " . $value . "<br>";
         } elseif ($key == 'sokhach') {
             echo "Số khách: " . $value . "<br>";
+        } elseif ($key == 'khunggio') {
+            echo "Khung giờ: " . $value . "<br>";
         } else {
-            // Các trường khác sẽ hiển thị bình thường
             echo $value . "<br>";
         }
     }
